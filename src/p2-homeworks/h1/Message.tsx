9c629db8ad646fa11c4972792ccd39a57f734189
gsricco <97520083+gsricco@users.noreply.github.com>
2022-04-30 23:45:45 +0300

    Add files via upload
    hw1

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import s from './Message.module.css';
+
+type MessagePropsType = {
+    avatar: string
+    message: string
+    name: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
+    return (
+        <div className={s.container}>
+            <div>
+                <img className={s.avatar} src={props.avatar} alt=''/>
+            </div>
+            <div className={s.mass}>
+                <div className={s.name}>{props.name}</div>
+
+                <div className={s.bro}>{props.message}</div>
+                <div className={s.time}>{props.time}</div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Message
